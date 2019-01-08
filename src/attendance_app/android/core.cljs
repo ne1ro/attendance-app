25d90d3fbcced2f1c58306b7a43706134746f5b8
neiro <neiro.mail@gmail.com>
2019-01-08 22:25:14 +0300

    Render attendants

--- src/attendance_app/android/core.cljs
+++ src/attendance_app/android/core.cljs
@@ -11,36 +11,32 @@
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def toolbar (r/adapt-react-class (.-ToolbarAndroid ReactNative)))
 (def flat-list (r/adapt-react-class (.-FlatList ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+(def image (r/adapt-react-class (.-Image ReactNative)))
 
 (def menu-img (js/require "./images/menu.png"))
-
-(defn alert [title]
-      (.alert (.-Alert ReactNative) title))
-
 (def colors
   {:primary "#3F51B5" :dark-primary "#303F9F" :light-primary "#C5CAE9" :accent "#FF5252"
    :text "#212121" :secondary-text "#757575" :divider "#BDBDBD"})
 
-; (defn attendant-row [{:id id}]
-;   [view {:style {:flex 1 :flex-direction "row"}}
-;    [view {:style {:flex 0.2 :background-color "red"}}
-;     [text {:style {:font-size 20 :text-align "center"}} "1"]]
+(defn alert [title] (.alert (.-Alert ReactNative) title))
 
-;    [view {:style {:flex 0.4 :background-color "powderblue"}}
-;     [text {:style {:font-size 20 :text-align "center"}} "First"]]
-
-;    [view {:style {:flex 0.4 :background-color "powderblue"}}
-;     [text {:style {:font-size 20 :text-align "center"}} "Last"]]])
+(defn attendant-row [{:id id :first-name first-name :last-name last-name}]
+  [view {:style {:flex 1 :flex-direction "row" :text-align "center"}}
+   [view {:style {:flex 0.2 :flex-direction "column"}} [text id]]
+   [view {:style {:flex 0.4 :flex-direction "column"}} [text first-name]]
+   [view {:style {:flex 0.4 :flex-direction "column"}}]] [text last-name])
 
 (defn app-root []
   (let [attendants (subscribe [:get-attendants])]
-    (fn [] [view {:style {:flex 1 :justify-content "flex-start" :color (:text colors)}}
+    (fn [] [view
+            {:style {:flex 1 :justify-content "flex-start" :color (:text colors)}}
       [toolbar
        {:title "Attendants"
         :title-color "#FFFFFF"
-        :navIcon menu-img
-        :style {:height 60 :background-color (:primary colors) :align-items "stretch"}}]])))
+        :nav-icon menu-img
+        :actions [{:title "Calendar"}]
+        :style {:height 48 :background-color (:primary colors) :align-items "stretch"}}]
+      [flat-list {:data #js[attendants] :render-item (fn [a] (-> a (attendant-row) (r/as-element)))}]])))
 
 (defn init []
       (dispatch-sync [:initialize-db])
