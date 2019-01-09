242611117ca8fe7a77114e8e3c6a9d53a3e2db39
neiro <neiro.mail@gmail.com>
2019-01-09 21:09:48 +0300

    Stylish attendants list

--- src/attendance_app/android/core.cljs
+++ src/attendance_app/android/core.cljs
@@ -25,9 +25,16 @@
 (defn attendant-row [a]
   (let [{id :id first-name :first-name last-name :last-name} a]
     [view {:style
-           {:flex 1 :flex-direction "row" :text-align "center" :align-self "stretch" :height 40 :padding 10 :padding-bottom 15}}
+           {:flex 1
+            :flex-direction "row"
+            :text-align "center"
+            :align-self "stretch"
+            :height 40
+            :padding-horizontal 20
+            :padding-vertical 10
+            :elevation 1}}
       [view {:style {:flex 0.2}}
-        [text {:style {:text-align "center" :font-weight "bold" :font-size 20}} (-> id (+ 1) str)]]
+        [text {:style {:font-weight "bold" :font-size 20}} (-> id (+ 1) str)]]
       [view {:style {:flex 0.4}}
         [text {:style {:font-size 20}} first-name]]
       [view {:style {:flex 0.4}}
@@ -36,14 +43,14 @@
 (defn app-root []
   (let [attendants (subscribe [:get-attendants])]
     (fn []
-      [view {:style {:flex 1 :justify-content "flex-start" :color (:text colors)}}
+      [view {:style {:flex 1 :justify-content "flex-start" :color (:text colors) :padding-bottom 20}}
 
       [toolbar
        {:title "Attendants"
         :title-color "#FFFFFF"
         :nav-icon menu-img
         :actions [{:title "Calendar"}]
-        :style {:height 48 :background-color (:primary colors) :align-items "stretch"}}]
+        :style {:height 48 :background-color (:primary colors) :align-items "stretch" :elevation 3}}]
 
       [flat-list
         {:data @attendants
