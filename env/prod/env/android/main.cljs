ca218be743a09b17bfdc655638028e1bbdcb2065
neiro <neiro.mail@gmail.com>
2019-01-28 12:51:07 +0300

    Fix prod config

--- env/prod/env/android/main.cljs
+++ env/prod/env/android/main.cljs
@@ -1,3 +1,4 @@
-Error detected while processing function cljfmt#AutoFormat[3]..cljfmt#Format[10]..<SNR>122_GetFormattedFile:
-line    4:
-E117: Unknown function: fireplace#session_eval
+(ns env.android.main
+  (:require [attendance-app.android.core :as core]))
+
+(core/init)
